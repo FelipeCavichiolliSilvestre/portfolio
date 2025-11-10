fb6ea554745d513c61f351f21d52dc208bed44d2
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2025-11-10 17:08:25 -0300

    added projects section on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,55 @@
 import { Navbar } from "@/components/Navbar";
 import { HeroBanner } from "@/components/HeroBanner";
+import { Marquee } from "@/components/ui/Marquee";
+import { Container } from "@/components/ui/Container";
+import { ProjectCard } from "@/components/ProjectCard";
 
 export default function Home() {
   return (
     <>
       <Navbar />
       <HeroBanner />
+
+      <span id="projetos" />
+      <Marquee items={Array(14).fill("Projetos")} />
+
+      <Container
+        className="cyan-theme bg-background flex flex-row flex-wrap justify-center items-stretch gap-8 lg:gap-16 lg:gap-y-8 py-8"
+        asChild
+      >
+        <section>
+          <div className="w-full sm:w-[calc(50%-(var(--spacing)*8/2))] md:w-[calc(33.33%-(var(--spacing)*8*2/3))] lg:w-[calc(33.33%-(var(--spacing)*16*2/3))]">
+            <ProjectCard
+              title="Yet Another Todo"
+              description="Um projeto de uma lista de afazeres com colaboração em tempo real utilizando Websockets e API REST. Feito com React + NestJS + SocketIO."
+              bannerSrc="/yat-banner.png"
+              bannerAlt="Uma captura de tela referente ao projeto 'Yet Another Todo'"
+              githubLink="https://github.com/FelipeCavichiolliSilvestre/yat"
+              projectLink="https://yat.felipesilvestre.dev/login"
+            />
+          </div>
+          <div className="w-full sm:w-[calc(50%-(var(--spacing)*8/2))] md:w-[calc(33.33%-(var(--spacing)*8*2/3))] lg:w-[calc(33.33%-(var(--spacing)*16*2/3))]">
+            <ProjectCard
+              title="Game of Life"
+              description="Um dos meus primeiros projetos. Simula o jogo da vida de John Conway, ao mesmo tempo que permite a alteração nas regras básicas de reprodução e sobrevivência."
+              bannerSrc="/game-of-life-banner.png"
+              bannerAlt="Uma captura de tela referente ao projeto 'Game of Life'"
+              githubLink="https://github.com/FelipeCavichiolliSilvestre/GameOfLife"
+              projectLink="https://life.felipesilvestre.dev"
+            />
+          </div>
+          <div className="w-full sm:w-[calc(50%-(var(--spacing)*8/2))] md:w-[calc(33.33%-(var(--spacing)*8*2/3))] lg:w-[calc(33.33%-(var(--spacing)*16*2/3))]">
+            <ProjectCard
+              title="Portfólio"
+              description="Você está aqui! Esta página em estilo neo-brutalismo teve seu design criado utilizando Figma e foi codificado utlizando NextJS + Shadcn + Tailwind CSS."
+              bannerSrc="/portfolio-banner.png"
+              bannerAlt="Uma captura de tela referente ao projeto 'Portfolio'"
+              githubLink="https://github.com/FelipeCavichiolliSilvestre/portfolio"
+              projectLink="#top"
+            />
+          </div>
+        </section>
+      </Container>
     </>
   );
 }
---
+++ components/ProjectCard.tsx
@@ -0,0 +1,61 @@
+import { GithubIcon, SquareArrowOutUpRight } from "lucide-react";
+import Image from "next/image";
+import { Button } from "@/components/ui/Button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/Card";
+
+type ProjectCardProps = {
+  title: string;
+  description: string;
+  bannerSrc: string;
+  bannerAlt: string;
+  githubLink: string;
+  projectLink: string;
+};
+
+function ProjectCard(props: ProjectCardProps) {
+  const { bannerAlt, bannerSrc, title, description, githubLink, projectLink } =
+    props;
+
+  return (
+    <Card className="bg-white w-full h-full">
+      <Image
+        src={bannerSrc}
+        alt={bannerAlt}
+        width={1600}
+        height={800}
+        className="w-full -mt-6 aspect-2"
+      />
+      <CardHeader>
+        <CardTitle>{title}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <CardDescription>{description}</CardDescription>
+      </CardContent>
+      <CardFooter className="inline-flex gap-4 justify-end items-end grow">
+        <Button size="icon" asChild>
+          <a href={githubLink} target="_blank">
+            <GithubIcon />
+          </a>
+        </Button>
+        <Button asChild>
+          <a
+            href={projectLink}
+            target={projectLink.startsWith("#") ? "_self" : "_blank"}
+          >
+            Visitar{projectLink === "#top" ? "?" : ""}
+            <SquareArrowOutUpRight />
+          </a>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
+
+export { ProjectCard };
