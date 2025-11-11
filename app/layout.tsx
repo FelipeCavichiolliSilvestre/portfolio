6d9d1b40795917fe98cbe99436c2e1c9bef3d981
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2025-11-11 16:02:47 -0300

    added open graph support

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,21 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Felipe Silvestre | Desenvolvedor Fullstack",
   description:
-    "Sou Felipe Cavichiolli Silvestre, um desenvolvedor fullstack. Seja bem vindo ao meu portfólio",
+    "Sou Felipe Cavichiolli Silvestre, um desenvolvedor fullstack. Seja bem vindo ao meu portfólio!",
+  openGraph: {
+    type: "website",
+    url: "https://felipesilvestre.dev",
+    title: "Felipe Silvestre | Desenvolvedor Fullstack",
+    description:
+      "Sou Felipe Cavichiolli Silvestre, um desenvolvedor fullstack. Seja bem vindo ao meu portfólio!",
+    images: [
+      {
+        url: "https://felipesilvestre.dev/opengraph-image.png",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
