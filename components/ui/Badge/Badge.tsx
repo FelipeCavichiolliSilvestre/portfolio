36df56efdaadd4c93ae5c997203b880e0266b739
FelipeCavichiolliSilvestre <felipecavichiollisilvestre@gmail.com>
2026-05-04 19:53:52 -0300

    added color variants to badge

--- components/ui/Badge/Badge.tsx
+++ components/ui/Badge/Badge.tsx
@@ -9,20 +9,25 @@ const badgeVariants = cva(
   "inline-flex items-center justify-center rounded-base border-2 border-border px-2.5 py-0.5 text-xs font-base w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] overflow-hidden",
   {
     variants: {
-      variant: {
+      color: {
         default: "bg-main text-main-foreground",
         neutral: "bg-secondary-background text-foreground",
+        green: "bg-chart-1 text-main-foreground",
+        blue: "bg-chart-2 text-main-foreground",
+        purple: "bg-chart-3 text-main-foreground",
+        pink: "bg-chart-4 text-main-foreground",
+        yellow: "bg-chart-5 text-main-foreground",
       },
     },
     defaultVariants: {
-      variant: "default",
+      color: "default",
     },
   },
 );
 
 function Badge({
   className,
-  variant,
+  color,
   asChild = false,
   ...props
 }: React.ComponentProps<"span"> &
@@ -34,7 +39,7 @@ function Badge({
   return (
     <Comp
       data-slot="badge"
-      className={cn(badgeVariants({ variant }), className)}
+      className={cn(badgeVariants({ color }), className)}
       {...props}
     />
   );
